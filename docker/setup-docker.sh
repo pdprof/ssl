08ee447f63316a5f0028b7e8fa3ad11306819c65
Kenichi Ooi <kenoi@jp.ibm.com>
2021-08-03 16:30:38 +0900

    change download trapit manually

--- docker/setup-docker.sh
+++ docker/setup-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O https://www.ibm.com/support/pages/system/files/support/swg/swgtech.nsf/0/d83af3cb5f0490d1852579d600618374/$FILE/trapit.002/trapit
+#curl -O https://www.ibm.com/support/pages/system/files/support/swg/swgtech.nsf/0/d83af3cb5f0490d1852579d600618374/$FILE/trapit.002/trapit
 if [ ! -f trapit ]; then
      echo "===================="
      echo "Please download trapit from https://www.ibm.com/support/pages/node/709009 and put it on the same directory."
